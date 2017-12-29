03a72e5290810c4b806be89ff0fb07fee9f5fe03
殷仁杰 <yinrenjia@ksjgs.com>
2017-12-29 23:48:21 +0800

    add checkstyle plugin, and do test it.

--- free-knight.monitor-httpool/src/main/java/com/framework/monitor/httpclient/exception/IllegalUrlException.java
+++ free-knight.monitor-httpool/src/main/java/com/framework/monitor/httpclient/exception/IllegalUrlException.java
@@ -1,11 +1,15 @@
+
 package com.framework.monitor.httpclient.exception;
 
-public class IllegalUrlException extends HttPoolException {
+public class IllegalUrlException
+		extends HttPoolException
+{
 
-	private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
-	public IllegalUrlException(String message) {
-		super(message);
+	public IllegalUrlException (
+			String message ) {
+		super ( message );
 	}
 
 }
